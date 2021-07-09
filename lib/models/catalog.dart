547f462f9927bc70b95b000932bb8c8b77108f37
Shriya Chandan <chandanshriya7@gmail.com>
2021-07-10 00:15:46 +0530

    🐛 Day 6

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -12,14 +12,21 @@ class CatalogModel {
 }
 
 class Item {
-  final int id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
+  final int? id;
+  final String? name;
+  final String? desc;
+  final int? price;
+  final String? color;
+  final String? image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  Item({
+    this.id,
+    this.name,
+    this.desc,
+    this.price,
+    this.color,
+    this.image,
+  });
 
   factory Item.fromMap(Map<String, dynamic> map) {
     return Item(
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -4,9 +4,7 @@ import 'package:flutter_catalog/models/catalog.dart';
 class ItemWidget extends StatelessWidget {
   final Item item;
 
-  const ItemWidget({Key? key, required this.item})
-      : assert(item != null),
-        super(key: key);
+  const ItemWidget({Key? key, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +13,9 @@ class ItemWidget extends StatelessWidget {
         onTap: () {
           print("${item.name} pressed");
         },
-        leading: Image.network(item.image),
-        title: Text(item.name),
-        subtitle: Text(item.desc),
+        leading: Image.network(item.image!),
+        title: Text(item.name!),
+        subtitle: Text(item.desc!),
         trailing: Text(
           "\$${item.price}",
           textScaleFactor: 1.5,
